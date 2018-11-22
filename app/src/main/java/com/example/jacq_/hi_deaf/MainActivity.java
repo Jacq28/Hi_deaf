5fe5b71d372af49ca78fa0c93a54bd3f8ec8124f
Jacq28 <39469507+Jacq28@users.noreply.github.com>
2018-11-22 14:24:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jacq_/hi_deaf/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jacq_.hi_deaf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
