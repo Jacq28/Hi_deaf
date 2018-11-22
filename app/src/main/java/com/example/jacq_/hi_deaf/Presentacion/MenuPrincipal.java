71b0a73818e31045a4d6a532235455a71cc01cf4
syz77 <sergiolagunac92@gmail.com>
2018-11-22 16:30:03 -0400

    Se crea la estructura para tres capas
    En la capa de presentacion se crea la actividad MenuPrincipal

---
+++ app/src/main/java/com/example/jacq_/hi_deaf/Presentacion/MenuPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.jacq_.hi_deaf.Presentacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
